d8e2aacade1c8edc48b526de151ab50e3fe56750
Skyler Austin <skyler.austin@outlook.com>
2022-04-14 00:05:13 -0700

    Testing vectorizer and model pickle load, vectorizing input, predicting sentiment. Left some commented out stuff to get it working (just testing)

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, url_for, render_template
 import pickle
 
-#load clf model
-clas=pickle.load(open('clf.plk', 'rb'))
-clf1= pickle.load(open('sentiment_mnb_vec.pickle', 'rb'))
+#load vectorizers
+vectorizer_saa6124 = pickle.load(open('..\\models\\vectorizer_saa6124.pickle', 'rb'))
 
-
+#load models
+#clas=pickle.load(open('clf.plk', 'rb'))
+sentiment_model_saa6124 = pickle.load(open('..\\models\\sentiment_saa6124.pickle', 'rb'))
 
 app = Flask(__name__)
 
@@ -18,12 +19,15 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    text= request.form["tweet_text"]
-    cyberbullying= request.form["cyberbullying_type"]
-    sentiment = request.form["cyberbullying_type"]
-    X_test_pkl=request.form["sentiment"]
-    cyberbullying=clas.predict(X_test_pkl)
-    sentiment= clf1.predict_proba(X_test_pkl)
-    return render_template("result.html", text= text, cyberbullying=cyberbullying, sentiment= sentiment)
+    text = request.form["tweet_text"]
+    text_vec = vectorizer_saa6124.transform([text])
+    sentiment_saa6124 = sentiment_model_saa6124.predict(text_vec)
+    # cyberbullying= request.form["cyberbullying_type"]
+    # sentiment = request.form["cyberbullying_type"]
+    # X_test_pkl=request.form["sentiment"]
+    #cyberbullying=clas.predict(X_test_pkl)
+    # sentiment= clf1.predict_proba(X_test_pkl)
+    #return render_template("result.html", text= text, cyberbullying=cyberbullying, sentiment = sentiment)
+    return render_template("result.html", text = text, sentiment = sentiment_saa6124)
 
 app.run(debug=True)
