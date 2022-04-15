2a09b38eb8838d45df890022838b7f6a1c427d24
Skyler Austin <skyler.austin@outlook.com>
2022-04-15 00:09:23 -0700

    Adding layout, cleaning up app.py

--- flask/app.py
+++ flask/app.py
@@ -1,33 +1,56 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, render_template
 import pickle
-
-#load vectorizers
-vectorizer_saa6124 = pickle.load(open('..\\models\\vectorizer_saa6124.pickle', 'rb'))
-
-#load models
-#clas=pickle.load(open('clf.plk', 'rb'))
-sentiment_model_saa6124 = pickle.load(open('..\\models\\sentiment_saa6124.pickle', 'rb'))
 
 app = Flask(__name__)
 
-# When a GET request is sent to the context "/"
+# load vectorizers
+vectorizer_saa6124 = pickle.load(open('..\\models\\vectorizer_saa6124.pickle', 'rb'))
+
+# load models
+#clas=pickle.load(open('clf.plk', 'rb'))
+sentiment_model_saa6124 = pickle.load(open('..\\models\\sentiment_saa6124.pickle', 'rb'))
+
+def get_sentiment(text):
+    text_vec_saa6124 = vectorizer_saa6124.transform([text])
+    prediction_prob_saa6124 = sentiment_model_saa6124.predict_proba(text_vec_saa6124)
+    prob_neg_saa6124 = prediction_prob_saa6124[0][0]
+    prob_pos_saa6124 = prediction_prob_saa6124[0][1]
+    # TODO: get predict_proba for other 2 sentiment models and replace below
+    prob_neg_avg = (prob_neg_saa6124 + prob_neg_saa6124 + prob_neg_saa6124) / 3
+    prob_pos_avg = (prob_pos_saa6124 + prob_pos_saa6124 + prob_pos_saa6124) / 3
+    if (prob_pos_avg > prob_neg_avg):
+        sentiment = {
+            "value": 1,
+            "label": "Positive",
+            "probability": round(prob_pos_avg * 100, 2)
+        }
+    else:
+        sentiment = {
+            "value": 0,
+            "label": "Negative",
+            "probability": round(prob_neg_avg * 100, 2)
+        }
+    return sentiment
+
+def get_cyberbullying(text):
+    # TODO
+    cyberbullying = {
+        "value": 0,
+        "label": "Not present",
+        "type": "N/A",
+        "probability": 100.00
+    }
+    return cyberbullying
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-
-
-@app.route("/predict", methods=["POST"])
-def predict():
+@app.route("/result", methods=["POST"])
+def result():
     text = request.form["tweet_text"]
-    text_vec = vectorizer_saa6124.transform([text])
-    sentiment_saa6124 = sentiment_model_saa6124.predict(text_vec)
-    # cyberbullying= request.form["cyberbullying_type"]
-    # sentiment = request.form["cyberbullying_type"]
-    # X_test_pkl=request.form["sentiment"]
-    #cyberbullying=clas.predict(X_test_pkl)
-    # sentiment= clf1.predict_proba(X_test_pkl)
-    #return render_template("result.html", text= text, cyberbullying=cyberbullying, sentiment = sentiment)
-    return render_template("result.html", text = text, sentiment = sentiment_saa6124)
+    sentiment = get_sentiment(text)
+    cyberbullying = get_cyberbullying(text)
+    return render_template("result.html", text=text, sentiment=sentiment, cyberbullying=cyberbullying)
 
 app.run(debug=True)
