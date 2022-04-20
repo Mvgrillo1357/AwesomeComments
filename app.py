be27e53e604584ddaaec9f2356c407bf17090df5
Michael Grillo <58634459+Mvgrillo1357@users.noreply.github.com>
2022-04-19 23:56:42 -0400

    ensemble learning for cyberbullying working as intended
    lgtm

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, render_template
-from model_init import ModelWithVectorizer, SequentialModel, CyberBullyModel, sentiment_labels
+from model_init import SentimentModel, SequentialModel, CyberBullyModel, sentiment_labels, cyberbully_labels
 
 app = Flask(__name__)
 
-saa6124 = ModelWithVectorizer('saa6124')
-cpb5703 = ModelWithVectorizer('cpb5703')
+saa6124 = SentimentModel('saa6124')
+cpb5703 = SentimentModel('cpb5703')
 #aav5195 = SequentialModel('aav5195')
-#mvg5906 = CyberBullyModel('mvg5906')
-#mvg5906_sgd = CyberBullyModel('sgdmvg5906')
+mvg5906 = CyberBullyModel('mvg5906')
+mvg5906_sgd = CyberBullyModel('sgdmvg5906')
 
 def get_ensemble_prediction(probabilities, labels):
     probabilities_ensemble = sum(probabilities)
@@ -31,14 +31,23 @@ def get_sentiment(text):
         sentiment_labels)
 
 
-# def get_cyberbullying(text):
-    # TODO
-    #cyberbullying1 = mvg5906.get_ctype(text)
-    #yberbullying2 = mvg5906_sgd.get_ctype(text)
-    #ensemble_cyber = (cyberbullying1 + cyberbullying2)/2
-    #cyberbullying = mvg5906_sgd.get_ctype(text)
-    # return cyberbullying
-
+def get_ensemble_cyberbullying(probabilities, labels):
+    probabilities_ensemble = sum(probabilities)
+    probabilities_ensemble = probabilities_ensemble / probabilities_ensemble.size
+    probability = probabilities_ensemble.max()
+    prediction = probabilities_ensemble.tolist().index(probability)
+    label = labels[prediction]
+    return {
+        "value": prediction,
+        "label": label,
+        "probability": round(probabilities.max() *100, 2)
+    }
+def get_sentiment_cyber(text):
+    probabilities_mvg5906 = mvg5906.get_probabilities(text)
+    probabilities_mvg5906_sgd = mvg5906_sgd.get_probabilities(text)
+    return get_ensemble_prediction(
+        [probabilities_mvg5906, probabilities_mvg5906_sgd], 
+        cyberbully_labels)
 
 @app.route("/", methods=["GET"])
 def index():
@@ -49,8 +58,8 @@ def index():
 def result():
     text = request.form["tweet_text"]
     sentiment = get_sentiment(text)
-    #cyberbullying = get_cyberbullying(text)
-    return render_template("result.html", text=text, sentiment=sentiment, cyberbullying={})
+    cyberbullying = get_sentiment_cyber(text)
+    return render_template("result.html", text=text, sentiment=sentiment, cyberbullying=cyberbullying)
 
 
 app.run(debug=True)
--- model_init.py
+++ model_init.py
@@ -6,8 +6,7 @@ from keras.preprocessing.sequence import pad_sequences
 
 sentiment_labels = {
     0: "Negative",
-    1: "Positive",
-    4: "Positive"
+    1: "Positive"
 }
 
 # SENTIMENT
@@ -17,12 +16,18 @@ NEUTRAL = "NEUTRAL"
 SENTIMENT_THRESHOLDS = (0.4, 0.7)
 
 cyberbully_labels = {
-    "age" : "Ageism",
-    "ethnicity" : "Racism",
-    "gender" : "Sexism",
-    "not_cyberbullying" : "Not Cyberbullying",
-    "other_cyberbullying" : "Another Type of Cyberbullying",
-    "religion" : "Anti Religious"
+    #"age" 
+    0 : "Ageism",
+    #"ethnicity" 
+    1 : "Racism",
+    #"gender" 
+    2 : "Sexism",
+    #"not_cyberbullying" 
+    3: "Not Cyberbullying",
+    #"other_cyberbullying" 
+    4: "Another Type of Cyberbullying",
+    #"religion" 
+    5: "Anti Religious"
 }
 
 class SequentialModel:
@@ -56,21 +61,12 @@ class SentimentModel:
 
 
 class CyberBullyModel:
-    def __init__(self, model_name1, model_name2):
-        vec_file1 = os.path.join(os.path.dirname(__file__), 'models', f'cyberbullyvectorizer_{model_name1}.pickle')
+    def __init__(self, model_name):
+        vec_file = os.path.join(os.path.dirname(__file__), 'models', f'cyberbullyvectorizer_{model_name}.pickle')
         self.vectorizer = pickle.load(open(vec_file, 'rb'))
-        model_file1 = os.path.join(os.path.dirname(__file__), 'models', f'cyberbullysentiment_{model_name1}.pickle')
+        model_file = os.path.join(os.path.dirname(__file__), 'models', f'cyberbullysentiment_{model_name}.pickle')
         self.model = pickle.load(open(model_file, 'rb'))
-        vec_file2 = os.path.join(os.path.dirname(__file__), 'models', f'cyberbullyvectorizer_{model_name2}.pickle')
-        self.vectorizer = pickle.load(open(vec_file, 'rb'))
-        model_file2 = os.path.join(os.path.dirname(__file__), 'models', f'cyberbullysentiment_{model_name2}.pickle')
-        self.model = pickle.load(open(model_file, 'rb'))
-    def get_ctype(self, text):
+    def get_probabilities(self, text):
         text_vectorized = self.vectorizer.transform([text])
-        prediction = self.model.predict(text_vectorized)[0]
         probabilities = self.model.predict_proba(text_vectorized)[0]
-        return {
-            "value": prediction,
-            "label": cyberbully_labels[prediction],
-            "probability": round(probabilities.max() *100, 2)
-        }
+        return probabilities
